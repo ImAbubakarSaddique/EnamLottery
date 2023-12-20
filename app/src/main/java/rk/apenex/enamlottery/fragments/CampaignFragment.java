aac3369ec3a547cbb8d217239f741721f44e9832
Abu Bakar Saddique <chabubakar726@gmail.com>
2023-12-21 03:25:06 +0500

    Work Done Till API

--- app/src/main/java/rk/apenex/enamlottery/fragments/CampaignFragment.java
+++ app/src/main/java/rk/apenex/enamlottery/fragments/CampaignFragment.java
@@ -1,66 +1,67 @@
 package rk.apenex.enamlottery.fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 import rk.apenex.enamlottery.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CampaignFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CampaignFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CampaignFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CampaignFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CampaignFragment newInstance(String param1, String param2) {
-        CampaignFragment fragment = new CampaignFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private ExtendedFloatingActionButton addNewCampaign_fbtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_campaign, container, false);
+        View v = inflater.inflate(R.layout.fragment_campaign, container, false);
+
+        addNewCampaign_fbtn = v.findViewById(R.id.addNewCampaign_fbtn);
+
+        addNewCampaign_fbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.add_new_campaign_dialog);
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.setCancelable(false);
+
+                AppCompatButton
+                        submit_btn = dialog.findViewById(R.id.submit_btn),
+                        cancel_btn = dialog.findViewById(R.id.cancel_btn);
+
+                Spinner dropdown = dialog.findViewById(R.id.spinner1);
+                String[] items = new String[]{"Subscribe a Youtube Channel","Watch a Youtube Video", "Follow an Instagram Account", "Like a Facebook Page", "Download an App", "Complete a Survey", "Watch a Video Ad", "Custom"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+                dropdown.setAdapter(adapter);
+
+                submit_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                cancel_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
+
+        return v;
     }
 }
